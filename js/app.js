8fe96ae6b36f962be749ba97158c3ed4b8bf80dd
Thea <tmush2014@agruni.edu.ge>
2018-09-18 18:50:41 +0400

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 new Vue({
     el: '#app',
     data: {
+        cart: {
+            items: []
+        },
         products: [
             {
                 id: 1,
@@ -46,6 +49,29 @@ new Vue({
             }
         ]
     }, 
+    methods: {
+      addProductToCart: function(product){
+        this.cart.items.push({
+            product: product,
+            quantity: 1
+        });
+
+        product.inStock--;
+      }
+    },
+    computed: {
+        cartTotal: function(){
+          var total = 0;
+
+          this.cart.items.forEach( function(item) {
+              total += item.quantity * item.product.price;
+
+          });
+
+          return total;
+        }
+    },
+
     filters: {
         currency: function(value){
             var formatter = Intl.NumberFormat('en-US', {
